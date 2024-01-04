044391d402e888d8d93337ae4a03985840b89f87
Tim Schneider <tim.schneider94@t-online.de>
2024-01-04 15:13:27 +0100

    Increased version number to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="natnet-client",
-    version="0.1",
+    version="0.1.1",
     author="Tim Schneider",
     author_email="schneider@ias.informatik.tu-darmstadt.de",
     description="Python client for Optitrack NatNet streams.",
